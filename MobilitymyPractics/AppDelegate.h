73ded7c641eb1cdca9f8528938fcb18b9722d7c5
saurabh <saurabhs@vmokshagroup.com>
2016-08-08 14:56:02 +0530

    Initial Commit

---
+++ MobilitymyPractics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobilitymyPractics
+//
+//  Created by Saurabh on 8/8/16.
+//  Copyright © 2016 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobilitymyPractics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobilitymyPractics
+//
+//  Created by Saurabh on 8/8/16.
+//  Copyright © 2016 vmoksha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
