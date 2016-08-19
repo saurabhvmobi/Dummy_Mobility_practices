c7f3ff5f9592f3b7e5f59254143c8fb1f6eef387
saurabh <saurabhs@vmokshagroup.com>
2016-08-19 11:12:06 +0530

    commit

--- MobilitymyPractics/ViewController.m
+++ MobilitymyPractics/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
-
+#import "UserInfo.h"
 @interface ViewController ()
+{
+    UserInfo *userInfo;
+
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *fullNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *emailIDLabel;
+@property (weak, nonatomic) IBOutlet UILabel *corpIDLabel;
 
 @end
 
@@ -17,11 +25,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    userInfo =[UserInfo shareUserInfo];
+    
+    [self updateprofileData];
+
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)upgradeButtonAction:(id)sender {
+
+    [self performSegueWithIdentifier:@"upgradeSegue" sender:self];
+}
+- (IBAction)tipsButtonAction:(id)sender {
+    [self performSegueWithIdentifier:@"tipsSegua" sender:self];
+
+}
+
+- (IBAction)bookARoomButtonAction:(id)sender {
+    [self performSegueWithIdentifier:@"bookaRoomSegua" sender:self];
+}
+
+
+
+
+-(void)updateprofileData
+{
+    self.emailIDLabel.text = userInfo.emailIDValue;
+    self.fullNameLabel.text = userInfo.fullName;
+    self.corpIDLabel.text = userInfo.cropID;
+
+
+}
+
 
 @end
